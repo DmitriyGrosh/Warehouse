a2edc7529c29f76e7be9a2d45a82f89f77ad3826
DmitriyGrosh <dmitriy.grosh@gmail.com>
2021-08-26 21:46:58 +0300

    added logical for "add warehouse page"

--- src/components/warehouses/AddWarehouse.js
+++ src/components/warehouses/AddWarehouse.js
@@ -1,5 +1,10 @@
 import React from 'react'
-import {Box, Container, makeStyles, TextField, Typography} from "@material-ui/core";
+import {Box, Button, Container, FormControl, makeStyles, TextField, Typography} from "@material-ui/core";
+import { useForm } from "react-hook-form";
+import {useDispatch, useSelector} from "react-redux";
+import {addWarehouse} from "../../redux/actions/warehouses"
+
+import TextError from "../main/TextError";
 
 const useStyles = makeStyles({
   box: {
@@ -7,10 +12,36 @@ const useStyles = makeStyles({
     gridGap: '20px',
     gridTemplateColumns: '1fr',
     paddingTop: '20px'
+  },
+  textField: {
+    width: '100%'
   }
 })
 
 const AddWarehouse = () => {
+
+  const dispatch = useDispatch()
+
+  const {getValues, register, trigger, formState: { errors }} = useForm()
+  const warehouses = useSelector(state => state.warehouse)
+
+  const handleAddWarehouse = () => {
+    const preCheckedValues = ["location", "area", "name"]
+
+    trigger(preCheckedValues).then(isValid => {
+      const formData = getValues()
+      const {location, area, name} = formData
+      const newWarehouse = {
+        idWareHouse: warehouses.length + 1,
+        location,
+        area,
+        products: [],
+        name
+      }
+
+      isValid && dispatch(addWarehouse(newWarehouse))
+    })
+  }
 
   const classes = useStyles()
   return (
@@ -19,21 +50,48 @@ const AddWarehouse = () => {
         Add Warehouse for your products
       </Typography>
       <Box className={classes.box}>
-        <TextField
-          label="Location"
-          type="text"
-          variant="outlined"
-        />
-        <TextField
-          label="Area (m^2)"
-          type="text"
-          variant="outlined"
-        />
-        <TextField
-          label="Name"
-          type="text"
-          variant="outlined"
-        />
+        <Box>
+          <TextField
+            className={classes.textField}
+            label="Location"
+            type="text"
+            variant="outlined"
+            inputProps={{
+              ...register("location", {required: true})
+            }}
+            error={!!errors.location}
+          />
+          {!!errors.location && <TextError>You should write location of warehouse</TextError>}
+        </Box>
+        <Box>
+          <TextField
+            className={classes.textField}
+            label="Area (m^2)"
+            type="number"
+            variant="outlined"
+            inputProps={{
+              ...register("area", {required: true})
+            }}
+            error={!!errors.area}
+          />
+          {!!errors.area && <TextError>You should write number of area in warehouse</TextError>}
+        </Box>
+        <Box>
+          <TextField
+            className={classes.textField}
+            label="Name"
+            type="text"
+            variant="outlined"
+            inputProps={{
+              ...register("name", {required: true})
+            }}
+            error={!!errors.name}
+          />
+          {!!errors.name && <TextError>You should write name of warehouse</TextError>}
+        </Box>
+          <Button onClick={handleAddWarehouse} variant="outlined">
+            Add Warehouse
+          </Button>
       </Box>
     </Container>
   )
--- src/redux/actions/warehouses.js
+++ src/redux/actions/warehouses.js
@@ -1,9 +1,17 @@
 import {ADD_PRODUCT} from "../types/warehouse";
+import {ADD_WAREHOUSE} from "../types/warehouse";
 
 export const addProduct = (value) => {
-  console.log('==========>value', value)
   return {
     type: ADD_PRODUCT,
+    value
+  }
+}
+
+export const addWarehouse = (value) => {
+  console.log('==========>value', value)
+  return {
+    type: ADD_WAREHOUSE,
     value
   }
 }
